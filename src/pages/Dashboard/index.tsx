184e70213c8d2d34eb1bd67f08a34d7020d74ace
doravantebeto <roberto.araujo339@gmail.com>
2020-12-27 19:45:54 -0300

    Dashboard Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import { FiPower } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { user, signOut } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
